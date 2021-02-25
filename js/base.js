fea2324197ae93a2eb33f5f19ca4ddaa5c0e7b5d
D.M <578328962@qq.com>
2021-02-25 21:13:25 +0800

    更换头部

--- js/base.js
+++ js/base.js
@@ -1,12 +1,12 @@
-var spanEle = document.getElementsByTagName('nav')[0].children;
-for(let i=0;i<spanEle.length;i++){
-	spanEle[0].style.color = '#7FDBFF';
-	spanEle[i].addEventListener('click',function(){
-		for(let j=0; j<spanEle.length;j++){
-			spanEle[j].style.color = '';
-			spanEle[j].style.borderBottom = 'none';
-		}
-		this.style.color = '#7FDBFF';
-		this.style.borderBottom = '2px solid #7FDBFF';
-	})
-}
+// var spanEle = document.getElementsByTagName('nav')[0].children;
+// for(let i=0;i<spanEle.length;i++){
+// 	spanEle[0].style.color = '#7FDBFF';
+// 	spanEle[i].addEventListener('click',function(){
+// 		for(let j=0; j<spanEle.length;j++){
+// 			spanEle[j].style.color = '';
+// 			spanEle[j].style.borderBottom = 'none';
+// 		}
+// 		this.style.color = '#7FDBFF';
+// 		this.style.borderBottom = '2px solid #7FDBFF';
+// 	})
+// }
